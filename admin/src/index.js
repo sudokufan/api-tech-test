4adabc9f3be6b0b5af293d2edf04b4817f0b7965
Alex Day <aday@hey.com>
2023-08-30 18:10:30 +0100

    updated deps, refactored /investments with axios

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,28 +1,31 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const config = require("config")
-const request = require("request")
+const express = require("express");
+const bodyParser = require("body-parser");
+const config = require("config");
+const request = require("request");
+const axios = require("axios");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json({limit: "10mb"}))
+app.use(bodyParser.json({ limit: "10mb" }));
 
-app.get("/investments/:id", (req, res) => {
-  const {id} = req.params
-  request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
-    if (e) {
-      console.error(e)
-      res.send(500)
-    } else {
-      res.send(investments)
-    }
-  })
-})
+app.get("/investments/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await axios.get(
+      `${config.investmentsServiceUrl}/investments/${id}`
+    );
+    res.send(response.data[0]);
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
 
 app.listen(config.port, (err) => {
   if (err) {
-    console.error("Error occurred starting the server", err)
-    process.exit(1)
+    console.error("Error occurred starting the server", err);
+    process.exit(1);
   }
-  console.log(`Server running on port ${config.port}`)
-})
+  console.log(`Server running on port ${config.port}`);
+});
